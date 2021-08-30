3f248d85f74d61fd1630bdb6ed0f2efd06b198f1
Siyona Lodha <siyonalodha@gmail.com>
2021-08-30 09:21:16 +0530

    trying changes

---
+++ teaching/app_users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from app_users.forms import UserForm, UserProfileInfoForm
+# Create your views here.
+
+def index(request):
+    return HttpResponse("This is Home Page")
+
+def register(request):
+
+    registered = Flase
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'app_users/registration.html',
+                  {'registered': registered,
+                   'user_form': user_form,
+                   'profile_form': profile_form})
